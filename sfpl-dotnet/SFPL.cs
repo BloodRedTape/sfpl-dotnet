9c8fad2ecb8481d832660ef7b0e71a190e1fba2a
E1Hephaestus <gefestroyale@gmail.com>
2022-01-10 08:11:48 +0200

    Basic c++ api wrapper

---
+++ sfpl-dotnet/SFPL.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace SFPL
+{
+    public struct OutputParameters
+    {
+        public int ImageWidth;
+        public int ImageHeight;
+        public string PlotTitle;
+        public string XAxisName;
+        public string YAxisName;
+
+        public OutputParameters(int imageWidth = 1280, int imageHeight = 720, string plotTitle = "", string xAxisName = "", string yAxisName = "")
+        {
+            ImageWidth = imageWidth;
+            ImageHeight = imageHeight;
+            PlotTitle = plotTitle;
+            XAxisName = xAxisName;
+            YAxisName = yAxisName;
+        }
+    }
+    public struct DataSource
+    {
+        public double[] X;
+        public double[] Y;
+        public string Name;
+    }
+
+    public class PlotBuilder
+    {
+        public static unsafe bool Build(DataSource[] sources, string outFilePath, OutputParameters parameters)
+        {
+            var xs = new double *[sources.Length];
+            var ys = new double *[sources.Length];
+            var sizes = new int[sources.Length];
+            var names = new string[sources.Length];
+
+            var dataHandles = new List<GCHandle>();
+
+            for(var i = 0; i<sources.Length; i++)
+            {
+                var xHandle = GCHandle.Alloc(sources[i].X, GCHandleType.Pinned);
+                var yHandle = GCHandle.Alloc(sources[i].Y, GCHandleType.Pinned);
+                dataHandles.Add(xHandle);
+                dataHandles.Add(yHandle);
+                
+                xs[i] = (double*)xHandle.AddrOfPinnedObject();
+                ys[i] = (double*)yHandle.AddrOfPinnedObject();
+                sizes[i] = Math.Min(sources[i].X.Length, sources[i].Y.Length);
+                names[i] = sources[i].Name;
+            }
+
+            bool result = PlotBuilder_BuildImplNative(xs, ys, sizes, names, sources.Length, outFilePath, 
+                parameters.ImageWidth, parameters.ImageHeight, parameters.PlotTitle, parameters.XAxisName, parameters.YAxisName);
+
+            foreach (var handle in dataHandles)
+                handle.Free();
+            
+            return result;
+        }
+
+        public static bool Build(DataSource source, string outFilePath, OutputParameters parameters)
+        {
+            return Build(new []{source}, outFilePath, parameters);
+        }
+
+        [DllImport(@"W:\Dev\sfpl-dll\cmake-build-debug\sfpl_dll.dll", CallingConvention = CallingConvention.StdCall)]
+        private static extern unsafe bool PlotBuilder_BuildImplNative(
+            double*[] xs,
+            double*[] ys, 
+            int[] sizes,
+            string[] names, 
+            int count, 
+            string outFilePath, 
+            int imageWidth, int imageHeight, 
+            string plotTitle, 
+            string xAxisName, 
+            string yAxisName);
+    }
+}
---
+++ tests/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SFPL;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int size = 10;
+        DataSource source;
+        source.X = new double[size];
+        source.Y = new double[size];
+        source.Name = "Test";
+
+        for (int i = 0; i < size; i++)
+        {
+            source.X[i] = i;
+            source.Y[i] = i * i;
+        }
+
+        if(!PlotBuilder.Build(source, "test.jpg", new OutputParameters(1280, 720, "Test")))
+            Console.WriteLine("Error");
+    }
+}
